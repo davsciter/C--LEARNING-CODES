71fb2eae328902d55fc752d2ccc1a8aeb66f2216
Davi Sena <senagx@hotmail.com>
2021-10-06 13:33:51 -0300

    Add files via upload

---
+++ Programs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__Learn
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mundo!");
+            Console.ReadLine();
+        }
+    }
+}
